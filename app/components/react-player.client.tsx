d1d31ae0d8b09aa9e56376ad19d40410052bb3ea
doc2288 <tata6602tata@gmail.com>
2025-10-23 13:59:06 +0200

    Добавил конфигурацию для отключения кнопок Start/End в progress bar

--- app/components/react-player.client.tsx
+++ app/components/react-player.client.tsx
@@ -1,2 +1,23 @@
 import ReactPlayer from "react-player";
-export default ReactPlayer;
+
+// Обертка для ReactPlayer с настройками по умолчанию
+const CustomReactPlayer = (props: any) => {
+  return (
+    <ReactPlayer
+      {...props}
+      config={{
+        file: {
+          attributes: {
+            controlsList: 'nodownload nofullscreen noremoteplayback',
+            // Отключаем контекстное меню
+            onContextMenu: (e: Event) => e.preventDefault()
+          }
+        },
+        // Объединяем с переданной конфигурацией
+        ...props.config
+      }}
+    />
+  );
+};
+
+export default CustomReactPlayer;
