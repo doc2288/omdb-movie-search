d556083a2d890fde0ed4a906fc93d3bbee482984
doc2288 <tata6602tata@gmail.com>
2025-10-23 14:07:32 +0200

    Откат изменений: восстановлен оригинальный root.tsx без подключения CSS файла для React Player

--- app/root.tsx
+++ app/root.tsx
@@ -8,12 +8,10 @@ import {
 } from '@remix-run/react';
 import type { LinksFunction } from '@remix-run/node';
 import stylesheet from '~/styles/tailwind.css';
-import reactPlayerStyles from '~/styles/react-player.css';
 import { ThemeProvider } from '~/contexts/ThemeContext';
 
 export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: stylesheet },
-  { rel: 'stylesheet', href: reactPlayerStyles },
   {
     rel: 'preconnect',
     href: 'https://fonts.googleapis.com',
