42429a13296a15855b6c0bb5cc23795607941eef
doc2288 <tata6602tata@gmail.com>
2025-10-23 14:08:31 +0200

    Откат изменений: восстановлен оригинальный react-player.client.tsx

--- app/components/react-player.client.tsx
+++ app/components/react-player.client.tsx
@@ -1,23 +1,2 @@
 import ReactPlayer from "react-player";
-
-// Обертка для ReactPlayer с настройками по умолчанию
-const CustomReactPlayer = (props: any) => {
-  return (
-    <ReactPlayer
-      {...props}
-      config={{
-        file: {
-          attributes: {
-            controlsList: 'nodownload nofullscreen noremoteplayback',
-            // Отключаем контекстное меню
-            onContextMenu: (e: Event) => e.preventDefault()
-          }
-        },
-        // Объединяем с переданной конфигурацией
-        ...props.config
-      }}
-    />
-  );
-};
-
-export default CustomReactPlayer;
+export default ReactPlayer;
